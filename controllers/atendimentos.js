fbfe79856e67215812ec3b48841ca32c8a510572
User GCT <yuri.ouverney@hotmai.com>
2021-05-26 11:43:57 -0300

    quarto modulo

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,15 +1,15 @@
 const Atendimento = require('../models/atendimentos')
 
-module.exports = app => { 
+module.exports = app => {
     //fazendo requisição (GET) -- (rota, função)
     //req -> requisição (o que as pessoas enviaram)
     //res -> o que o servidor está devolvendo
     app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está realizando um GET'))
 
-    app.post('/atendimentos', (req, res) => {
-        const atendimento = req.body
 
-        Atendimento.adiciona(atendimento)
-        res.send('Post atendimento')
+    app.post('/atendimentos', (req, res) => {
+       const atendimento = req.body
+
+        Atendimento.adiciona(atendimento, res)
     })
 }
--- infraestrutura/tabelas.js
+++ infraestrutura/tabelas.js
@@ -5,7 +5,7 @@ class Tabelas{
     }
 
     criarAtendimentos(){
-        const sql = 'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, Client varchar(50) NOT NULL, Pet varchar(30), Servico varchar(20) NOT NULL, Status varchar(20) NOT NULL, Observacoes text, PRIMARY KEY(id))'
+        const sql = 'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, Client varchar(50) NOT NULL, Pet varchar(30), Servico varchar(20) NOT NULL, Status varchar(20) NOT NULL, Data datetime NOT NULL, dataCriacao datetime NOT NULL, Observacoes text, PRIMARY KEY(id))'
         
         this.conexao.query(sql, (erro) => {
             if (erro){
